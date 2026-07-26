79178fd484580fbec5581f09ba4da74ce78f76df
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-07-27 01:14:44 +0200

    Полная переработка учебного материала

--- modern-example/ordering.mjs
+++
@@ -1,5 +0,0 @@
-console.log('1: sync');
-queueMicrotask(() => console.log('3: microtask'));
-setTimeout(() => console.log('4: timer'), 0);
-await Promise.resolve();
-console.log('2: continuation after await');
