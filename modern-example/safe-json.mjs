275916ebee16e8bd06553f1a89016875e904fa52
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-07-27 01:14:44 +0200

    Полная переработка учебного материала

--- modern-example/safe-json.mjs
+++
@@ -1,8 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-export async function readJson(url) {
-  const source = await readFile(url, 'utf8');
-  return JSON.parse(source);
-}
-
-console.log(await readJson(new URL('./package.json', import.meta.url)));
