f055a666a4f088e42c8fb5883333f73808351ad6
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-07-27 01:14:44 +0200

    Полная переработка учебного материала

--- modern-example/permission-demo.mjs
+++
@@ -1,5 +0,0 @@
-import { readFile } from 'node:fs/promises';
-console.log(await readFile(new URL('../README.md', import.meta.url), 'utf8'));
-// Сравните:
-// node --permission permission-demo.mjs
-// node --permission --allow-fs-read=.. permission-demo.mjs
