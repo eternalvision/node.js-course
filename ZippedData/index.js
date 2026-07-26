89df61072277471e2c9e8658fc72ab81ac3a836c
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-07-27 00:50:25 +0200

    Глобальное обновление курса Node.js

--- ZippedData/index.js
+++
@@ -1,8 +0,0 @@
-const zlib = require("zlib");
-const fs = require("fs");
-
-let compressStream = zlib.createGzip();
-let readStream = fs.createReadStream("example.txt");
-let writeStream = fs.createWriteStream("Example.txt.gz");
-
-readStream.pipe(compressStream).pipe(writeStream);
