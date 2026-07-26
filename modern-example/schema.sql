0770598c47e9f452de60fe40651ff94270fbce8e
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-07-27 01:14:44 +0200

    Полная переработка учебного материала

--- modern-example/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tasks (
-  id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  title text NOT NULL CHECK (char_length(title) BETWEEN 1 AND 200),
-  completed boolean NOT NULL DEFAULT false,
-  created_at timestamptz NOT NULL DEFAULT now()
-);
