9109e1220f3f8d71ea596684d94211efe418f33c
s.p. <56647239+eternalvision@users.noreply.github.com>
2026-07-27 01:14:44 +0200

    Полная переработка учебного материала

--- modern-example/report.sql
+++
@@ -1,6 +0,0 @@
-EXPLAIN (ANALYZE, BUFFERS)
-SELECT u.id, u.name, count(t.id) AS open_tasks
-FROM users AS u
-LEFT JOIN tasks AS t ON t.user_id = u.id AND NOT t.completed
-GROUP BY u.id, u.name
-ORDER BY open_tasks DESC;
